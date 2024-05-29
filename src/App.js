3ed3c127cdf88841909f1aab8a14df9a8f35673c
michael-qu <qulubin2013@gmail.com>
2024-05-29 16:11:59 -0400

    Sign function added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import logo from './logo.svg';
-import { connect } from '@joyid/ckb';
+import * as React from 'react';
+import { connect, signChallenge } from '@joyid/ckb';
 import './App.css';
 
 function App() {
+  const [joyidInfo, setJoyidInfo] = React.useState(null);
+  const [challenge, setChallenge] = React.useState('Sign this for me');
+
   const onConnect = async () => {
     try {
       const authData = await connect();
+      setJoyidInfo(authData);
       console.log(`JoyID user info:`, authData);
     } catch (error) {
       console.error(error);
     }
   }
  
+  const onSign = async () => {
+    const res = await signChallenge(challenge, joyidInfo.address);
+    if (res) {
+      alert('Sign message successful');
+      console.log(`Sign message result: ${res}`);
+    }
+  }
+
   return (
     <div>
       <h1>Hello JoyID!</h1>
-      <button onClick={onConnect}>Connect JoyID</button>
+      {joyidInfo ? null : <button onClick={onConnect}>Connect JoyID</button>}
+      {joyidInfo ? (
+        <div>
+          <textarea value={challenge} onChange={e => setChallenge(e.target.value)} />
+          <button onClick={onSign}>Sign With JoyID</button>
+        </div>
+      ) : null}
     </div>
   );
 }
